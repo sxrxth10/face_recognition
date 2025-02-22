68ca4f2f8d224518a012c2e8563cafde289e8b32
Sarath Balu <sarathbalu829@gmail.com>
2025-02-23 01:22:23 +0530

    Updated backend URL to AWS public IP

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -4,7 +4,7 @@ import tempfile
 from PIL import Image
 from io import BytesIO
 
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "http://3.110.135.254:8000"
 
 st.title("Face Recognition System")
 
